80002062171715fbe435e3f75679629e9d81691f
Kerem Dede <kerem.dede@campus.tu-berlin.de>
2017-12-07 10:10:03 +0100

    Ikinci videoda islenenler

--- webserver.py
+++ webserver.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask, request
+from flask import Flask, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,5 +12,20 @@ def close():
     kapat = request.environ.get('werkzeug.server.shutdown')
     kapat()
 
+@app.route('/kisiler/')
+def kisiler():
+    mesaj = 'kisiler sayfasinda bulunuyorsun'
+    return mesaj
+
+@app.route('/profil/<int:identity>')
+def profil(identity):
+    mesaj = 'Suanda {} numarali profile bakiyorsun'.format(identity)
+    return mesaj
+
+@app.route('/liste')
+def liste():
+    return url_for('profil', identity=30)
+
+
 if __name__ == '__main__':
-    app.run(host='localhost', port=4444, debug=False)
+    app.run(host='localhost', port=4444, debug=True)
